0137b65776c9a6251d56ae693901100dab09fada
eun2u <hhaneunjoo@gmail.com>
2020-08-13 19:40:51 +0900

    페이지쪽수(꼬리말) 제거

--- pdftoxml.py
+++ pdftoxml.py
@@ -6,17 +6,48 @@ import kss
 
 #-*- encoding:utf-8
 from tika import parser
-PDFfileName = '출판사를 위한 전자책 길잡이.pdf'
+PDFfileName = 'test1.pdf'
 parsed = parser.from_file(PDFfileName, xmlContent=True)
 fileOut = open('fileOut.xml', 'w', encoding='utf-8')
 print(parsed['content'], file=fileOut)
+fileOut.close()
 
 from bs4 import BeautifulSoup
+with open("fileOut.xml", "r", encoding="utf-8") as b:
+    inputxml = b.read()
+soup = BeautifulSoup(inputxml, "xml") 
 
-soup = BeautifulSoup(open(fileOut), 'xml')
-#paragraph_list=soup.findAll('div',{"class"="page"})
+def remove_header(text):
+    start_i=-1
+    end_i=-1
+    for idx,i in enumerate(text):
+        if(i=='\n' and start_i!=-1):
+            end_i=idx
+            break
+        elif(i=='\n'):
+            start_i=idx
+    if(bool(re.search(r'\d',text[start_i:end_i]))==False):
+        return text
+    else:
+        return (text[0:start_i] + text[end_i:len(text)] )
 
+
+def no_space(text):
+    pattern1=re.compile('\n')
+    text=re.sub(pattern1, "", text)
+    return text
+
+fileOut = open('fileOut.txt', 'w', encoding='utf-8')
+paragraph_list = soup.find_all('div',{'class':'page'})
+tot_content=''
 for a in paragraph_list:
-    print a.get_text()
+    text=remove_header(a.get_text())
+    text=no_space(text)
+    tot_content=tot_content + text
 
-fileOut.close()
+for sent in kss.split_sentences(tot_content):
+    print(sent,file=fileOut)
+#print(tot_content,file=fileOut)
+
+#<p>tag 첫번째가 숫자를 포함하면, 첫번째 <p> tag remove
+fileOut.close
--- textRank.py
+++ textRank.py
@@ -4,31 +4,52 @@ from konlpy.tag import Kkma
 from konlpy.utils import pprint
 import kss
 
-def clean_text(text):
-    #cleaned_text = re.sub('[a-zA-z]','',text)
-    cleaned_text = re.sub('[\{\}\[\]\/;:|\)*~`^\-_+<>@\#$%&\\\=\(\'\"\♥\♡\ㅋ\ㅠ\ㅜ\ㄱ\ㅎ\ㄲ\ㅡ]','',text)
-    return cleaned_text
-
-def no_space(text):
-    pattern1=re.compile('\n\n')
-    text=re.sub(pattern1, " ", text)
-    pattern2=re.compile('\n')
-    text=re.sub(pattern2, "",text)
-    #text=re.sub('check','\n',text)
-    return text
-
 #-*- encoding:utf-8
 from tika import parser
-PDFfileName = 'test2.pdf'
-parsed = parser.from_file(PDFfileName)
-fileOut = open('fileOut.txt', 'w', encoding='utf-8')
-text=no_space(parsed['content'])
-text=clean_text(text)
-#print(parsed['content'], file=fileOut)
-#print(text,file=fileOut)
-for sent in kss.split_sentences(text):
-    print(sent,file=fileOut)
+PDFfileName = 'test1.pdf'
+parsed = parser.from_file(PDFfileName, xmlContent=True)
+fileOut = open('fileOut.xml', 'w', encoding='utf-8')
+print(parsed['content'], file=fileOut)
 fileOut.close()
+
+from bs4 import BeautifulSoup
+with open("fileOut.xml", "r", encoding="utf-8") as b:
+    inputxml = b.read()
+soup = BeautifulSoup(inputxml, "xml") 
+
+def remove_header(text):
+    start_i=-1
+    end_i=-1
+    for idx,i in enumerate(text):
+        if(i=='\n' and start_i!=-1):
+            end_i=idx
+            break
+        elif(i=='\n'):
+            start_i=idx
+    if(bool(re.search(r'\d',text[start_i:end_i]))==False):
+        return text
+    else:
+        return (text[0:start_i] + text[end_i:len(text)] )
+
+def no_space(text):
+    pattern1=re.compile('\n')
+    text=re.sub(pattern1, "", text)
+    return text
+
+fileOut = open('fileOut.txt', 'w', encoding='utf-8')
+paragraph_list = soup.find_all('div',{'class':'page'})
+tot_content=''
+for a in paragraph_list:
+    text=remove_header(a.get_text())
+    text=no_space(text)
+    tot_content=tot_content + text
+
+for sent in kss.split_sentences(tot_content):
+    print(sent,file=fileOut)
+#print(tot_content,file=fileOut)
+
+#<p>tag 첫번째가 숫자를 포함하면, 첫번째 <p> tag remove
+fileOut.close
 
 
 #textRank 
